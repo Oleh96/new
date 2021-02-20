b3c5732f62997a09ddebe5124fb1645e4865430b
Oleh <oleh.rudenko96@gmail.com>
2021-02-20 14:45:07 +0200

    first commit

---
+++ script6.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let number = 4;
+
+console.log(4/0);
+console.log('string' * 9);
+
+
+const person = "Alex";
+
+const bool = true;
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+
+// console.log(obj.name);
+console.log(obj ["name"]);
+
+
+
+let arr = ['plum.png','orange.jpg','apple.bpm', 6];
+
+console.log(arr[3]);
+
+
---
+++ script7.js
@@ -0,0 +1,24 @@
+"use strict";
+
+ // alert ('Hello'); 
+
+//const result = confirm ("Are you here?");
+//console.log(result);
+
+
+//const answer = prompt("вам є 18 ?", "18");
+// console.log(typeof(answer));
+
+//console.log(answer + 5);
+
+//const answer = +prompt("вам є 18 ?", "18");
+//console.log(answer + 5);
+
+
+const answers = [];
+
+answers[0]= prompt("Ваше ім'я ?", "");
+answers[1]= prompt("Ваше прізвище ?", "");
+answers[2]= prompt("Скільки вам років ?", "");
+
+console.log(typeof(answers));
